a8c389d2b9594956d383b3ea362d392b732dd152
Starinin Andrey <starininandrey@gmail.com>
2026-04-18 13:17:14 +0300

    Add history

--- Calculator/MainWindow.axaml.cs
+++ Calculator/MainWindow.axaml.cs
@@ -1,36 +1,45 @@
+using System.Collections.Generic;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-using Tmds.DBus.Protocol;
 
 namespace Calculator;
 
 public partial class MainWindow : Window
 {
+    private readonly List<string> _historyResults = [];
+    
     public MainWindow()
     {
         InitializeComponent();
+        
+        DataContext = this;
     }
 
     private void Button_Calc_OnClick(object? sender, RoutedEventArgs e)
     {
         if (!double.TryParse(Number1.Text ?? "", out var num1))
         {
-            Result.Text = $"Invalid number: {Number1?.Text}.";
+            StatusBar.Text = $"Invalid number: {Number1?.Text}.";
             return;
         }
         
         if (!double.TryParse(Number2.Text ?? "", out var num2))
         {
-            Result.Text = $"Invalid number: {Number2?.Text}.";
+            StatusBar.Text = $"Invalid number: {Number2?.Text}.";
             return;
         }
 
         var symbol = Symbol.Text;
-        Result.Text = symbol switch
+        var result = symbol switch
         {
             "+" => $"{num1} + {num2} = {num1 + num2}",
             "/" => $"{num1} / {num2} = {num1 / num2}",
             _ => "Invalid operation."
         };
+        
+        _historyResults.Add(result);
+        
+        History.Text = "";
+        History.Text = string.Join("\n",_historyResults);
     }
 }
